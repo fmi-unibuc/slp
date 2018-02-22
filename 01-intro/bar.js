725f7dff2001ade159ec065ee6f58dcc874f73cb
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-02-23 01:34:03 +0200

    Resetted to 2018 class

--- 01-intro/bar.js
+++
@@ -1,5 +0,0 @@
-function bar(x) { 
-  return function() { var x = 5; return x; }; 
-}
-var f = bar(200);
-console.log("%d", f());
--- 01-intro/bar2.js
+++
@@ -1,5 +0,0 @@
-function bar(x) { 
-  return function() { var x = x; return x; }; 
-}
-var f = bar(200);
-console.log("%d", f());
